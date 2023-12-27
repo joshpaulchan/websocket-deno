9cb7bf1a5b723aa33b0a8b31a14ae200f997934c
Joshua Paul Chan <joshpaulchan@gmail.com>
2023-12-26 22:44:29 -0500

    basic server

--- main.ts
+++ main.ts
@@ -1,5 +1,34 @@
 
+function health(_request: Request) {
+    return new Response(null)
+}
 
-Deno.serve((_request: Request) => {
-    return new Response("Hello, world!");
-});
+function readiness(_request: Request) {
+    return new Response(null)
+}
+
+function connections(_request: Request) {
+    return new Response(null)
+}
+
+function notFound(_request: Request) {
+    return new Response(null, {
+        status: 404
+    })
+}
+
+function router(routes) {
+    return async function handler(request) {
+        return (routes[new URL(request.url).pathname] ?? notFound)(request)
+    }
+}
+
+Deno.serve({
+    hostname: "0.0.0.0",
+    port: 8080,
+    handler: router({
+        "/healthz": health,
+        "/readiness": readiness,
+        "/connections": connections
+    })
+})
